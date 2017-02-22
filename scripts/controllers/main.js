763ea8b291e5b566ce203b181f9a0351ce4b8cd4
GitFyHub <Younus@mcbp3621.local>
2017-02-22 07:19:14 -0600

    Applied VS Code auto formatting

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,31 +1,31 @@
 'use strict';
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService) {
-  $scope.addTodo = function() {
-    //console.log("An input changed!");
-      var todo = {name: "This is a new todo"}
-      $scope.todos.unshift(todo);
-  };
+    .controller('mainCtrl', function ($scope, dataService) {
+        $scope.addTodo = function () {
+            //console.log("An input changed!");
+            var todo = { name: "This is a new todo" }
+            $scope.todos.unshift(todo);
+        };
 
-  $scope.helloWorld = dataService.helloWorld;
-  
-  dataService.getTodos(function(response) {
-      console.log(response.data);
-      $scope.todos = response.data;
-    });
-    
-    $scope.deleteTodo = function(todo, $index) {
-        dataService.deleteTodo(todo);
-        $scope.todos.splice($index,1);
-    };
-    
-    $scope.saveTodos = function() {
-        var filteredTodos = $scope.todos.filter(function(todo) {
-            if(todo.edited) {
-                return todo;
-            };
+        $scope.helloWorld = dataService.helloWorld;
+
+        dataService.getTodos(function (response) {
+            console.log(response.data);
+            $scope.todos = response.data;
         });
-        dataService.saveTodos(filteredTodos);
-    };    
-})
+
+        $scope.deleteTodo = function (todo, $index) {
+            dataService.deleteTodo(todo);
+            $scope.todos.splice($index, 1);
+        };
+
+        $scope.saveTodos = function () {
+            var filteredTodos = $scope.todos.filter(function (todo) {
+                if (todo.edited) {
+                    return todo;
+                };
+            });
+            dataService.saveTodos(filteredTodos);
+        };
+    })
